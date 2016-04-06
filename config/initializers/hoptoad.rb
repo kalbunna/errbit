5a9356f7f8f19558c97d96c7192ca4554ad265ab
kalbunna <kal.bunna@gmail.com>
2016-04-06 10:04:56 +0700

    ignore 404

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -3,21 +3,22 @@ HoptoadNotifier.configure do |config|
   # to set a dummy API key so that HoptoadNotifier doesn't complain.
   config.api_key = "---------"
 
-  # Don't log error that causes 404 page\
-  config.ignore_only = %w{
-    ActiveRecord::RecordNotFound
-    ActionController::RoutingError
-    ActionController::InvalidAuthenticityToken
-    CGI::Session::CookieStore::TamperedWithCookie
-    ActionController::UnknownAction
-    AbstractController::ActionNotFound
-    ActionView::MissingTemplate
-    Timeout::Error
-    Net::HTTPBadResponse
-    WebuiMatcher::InvalidRequestFormat
-    ActionController::UnknownFormat
-    ActivXML::Transport::NotFoundError
-    Mongoid::Errors::DocumentNotFound
-  }
+  # Don't log error that causes 404 page
+  config.ignore << "ActionController::RoutingError"
+  # config.ignore_only = %w{
+  #   ActiveRecord::RecordNotFound
+  #   ActionController::RoutingError
+  #   ActionController::InvalidAuthenticityToken
+  #   CGI::Session::CookieStore::TamperedWithCookie
+  #   ActionController::UnknownAction
+  #   AbstractController::ActionNotFound
+  #   ActionView::MissingTemplate
+  #   Timeout::Error
+  #   Net::HTTPBadResponse
+  #   WebuiMatcher::InvalidRequestFormat
+  #   ActionController::UnknownFormat
+  #   ActivXML::Transport::NotFoundError
+  #   Mongoid::Errors::DocumentNotFound
+  # }
 end
 
